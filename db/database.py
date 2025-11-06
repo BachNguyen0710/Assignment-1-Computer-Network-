f82cd664976bf399ef9fe459403671de219c2914
BachNguyen0710 <bach7102005@gmail.com>
2025-11-06 20:33:36 +0700

    fix db

--- db/database.py
+++ db/database.py
@@ -1,46 +1,46 @@
 import os
 import json
-import threading
 
-USER_DATABASE = {}
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(DIR_PATH, "data.json")
 
-SESSION_STORE = {}
-SESSION_LOCK = threading.Lock()
-
-ACTIVE_PEERS = {}
-PEERS_LOCK = threading.Lock()
-
-def load_json():
-    global USER_DATABASE
+def read_json():
     try:
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        json_path = os.path.join(dir_path, 'data.json')
-        with open(json_path, 'r') as f:
-            data = json.load(f)
-            USER_DATABASE = data.get("users", {})
-            print(f"[Database] Loaded users: {list(USER_DATABASE.keys())}")
+        with open(JSON_PATH, "r") as file:
+            return json.load(file)
     except Exception as e:
-        print(f"[DB] Can't load data.json: {e}")
-        print("[DB] Server run with no users.")
-        USER_DATABASE = {}
-
+        print(f"Error reading JSON file: {e}")
+        return {}
+def write_json(data):
+    try:
+        with open(JSON_PATH, "w") as file:
+            json.dump(data, file, indent=4)
+    except Exception as e:
+        print(f"Error writing JSON file: {e}")
 #USER: 
 def get_user_database(username):
-    return USER_DATABASE.get(username)
+    data = read_json()
+    return data.get("users", {}).get(username)
 
 #Session:
 def create_session(session_id, username):
-    with SESSION_LOCK:
-        SESSION_STORE[session_id] = username
+    data = read_json()
+    if "session_store" not in data:
+        data["session_store"] = {}
+    data["session_store"][session_id] = username
+    write_json(data)
 def get_username_by_session(session_id):
     if not session_id:
         return None
-    with SESSION_LOCK:
-        return SESSION_STORE.get(session_id)
+    data = read_json()
+    return data.get("session_store", {}).get(session_id)
 #Peer:
 def register_peer(username, ip, port):
-    with PEERS_LOCK:
-        ACTIVE_PEERS[username] = {"ip": ip, "port": port}
+    data = read_json()
+    if "active_peer" not in data:
+        data["active_peer"] = {}
+    data["active_peer"][username] = {"ip": ip, "port": port}
+    write_json(data)
 def get_peers():
-    with PEERS_LOCK:
-        return ACTIVE_PEERS.copy()
+    data = read_json()
+    return data.get("active_peer", {})
